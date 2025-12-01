f25d61ea35999f38b20793f0e28a68df90940f92
josefer <josefer@kali2.com>
2025-12-01 10:18:18 -0600

    cambiando sqlite-> postgres

--- api/settings.py
+++ api/settings.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv     # Para que lea variables de entorno (en desarrollo)
+import os
+
+load_dotenv()
+
+
 """
 Django settings for api project.
 
@@ -76,12 +82,27 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Descomentar si quiero usar la BD de sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
+
+
 
 
 # Password validation

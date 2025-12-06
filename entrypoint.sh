6d2d486c759fcb6ac3aa334f1e52eb0d6ebf8987
FErNANDoJOS99 <111204390+FErNANDoJOS99@users.noreply.github.com>
2025-12-06 16:35:37 -0600

    Update  entrypoint.sh add initial data to bd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 echo "Running Django migrations..."
 python manage.py migrate --noinput
 
+# Solo cuando este en modo inicial (de preferencia solo una vez)
+if [ "$LOAD_FIXTURES" = "true" ]; then
+  echo "Loading initial data..."
+  python manage.py loaddata datos.json
+fi
+
 echo "Starting supervisord..."
 exec "$@"
-
